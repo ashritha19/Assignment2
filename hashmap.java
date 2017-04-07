82d8c63f071ebb7e4521cf930d2fef9c7f1dfc64
ashritha19 <ashrithareddy19@gmail.com>
2017-04-06 22:39:45 -0400

    Update hashmap.java

--- hashmap.java
+++ hashmap.java
@@ -1,11 +1,11 @@
 import java.util.*;  
-class TestCollection13{  
+class Hashmap{  
  public static void main(String args[]){  
-  HashMap<Integer,String> hm=new HashMap<Integer,String>();  
-  hm.put(100,"Amit");  
-  hm.put(101,"Vijay");  
-  hm.put(102,"Rahul");  
-  for(Map.Entry m:hm.entrySet()){  
+  HashMap<Integer,String> hmap=new HashMap<Integer,String>();  
+  hmap.put(100,"Amit");  
+  hmap.put(101,"Vijay");  
+  hmap.put(102,"Rahul");  
+  for(Map.Entry m:hmap.entrySet()){  
    System.out.println(m.getKey()+" "+m.getValue());  
   }  
  }  
